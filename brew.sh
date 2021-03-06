a8680a37e369389d94f2a3794c4a79104fc4800e
Martin Degeldt <mde@synapsis-group.com>
2021-03-06 11:18:43 +0100

    adding config file and iterm pref

---
+++ brew.sh
@@ -0,0 +1,20 @@
+# Install Brew Packages
+brew install node
+brew install tree
+
+brew install neofetch
+brew install koekeishiya/formulae/yabai
+brew install koekeishiya/formulae/skhd
+
+brew install --cask karabiner-elements
+brew install --cask google-chrome
+brew install --cask stats
+brew install --cask iterm2
+brew install --cask messenger
+brew install --cask visual-studio-code
+brew install --cask microsoft-office
+brew install --cask sourcetree
+brew install --cask postman
+
+brew tap homebrew/cask-fonts
+brew install --cask font-fira-code

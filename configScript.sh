8c563f519fa9f9bfc8394ae880b1f46b1edd92fb
Martin Degeldt <martin.degeldt@gmail.com>
2021-03-14 11:13:09 +0100

    Update configScript.sh

--- configScript.sh
+++ configScript.sh
@@ -4,7 +4,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-mv .zshrc ~/.zshrc
+cp .zshrc ~/.zshrc
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 

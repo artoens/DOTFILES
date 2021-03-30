cfa9523a9f88f6181fd2835dc365fc7b0de18931
Martin Degeldt <mde@synapsis-group.com>
2021-03-30 21:38:27 +0200

    modif with first install

--- configScript.sh
+++ configScript.sh
@@ -1,4 +1,4 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
@@ -7,7 +7,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 cp .zshrc ~/.zshrc
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/artoens/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 sh brew.sh
 
-cp .config ~/.config
+cp -r .config ~/.config
